7c2cf60602a2ed4ac092eecbb418190afb919c9e
jazz-code <ren.meta@gmail.com>
2019-07-19 12:20:08 -0400

    fixed errors and mapped over all the names

--- starwars/src/components/StarWarsGrid.js
+++ starwars/src/components/StarWarsGrid.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import StarWarsCard from "./StarWarsCard";
 
 export default function StarWarsGrid() {
+  const [people, setPeople] = useState([]);
+
   useEffect(() => {
     axios.get(`https://swapi.co/api/people/`).then(res => {
       const people = res.data.results;
       console.log(people);
+      setPeople(people);
     });
   }, []);
 
-  return <div />;
+  return (
+    <div>
+      Test
+      {people.map(peopleArray => {
+        return <StarWarsCard key={peopleArray} starWars={peopleArray.name} />;
+      })}
+    </div>
+  );
 }
